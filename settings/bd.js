c76d1b0e593e74edde6b6f57c736a38a8ff78773
Daskendrik <ss599@yandex.ru>
2023-12-05 15:35:03 +0300

    поменяла названия

--- settings/bd.js
+++ settings/bd.js
@@ -3,11 +3,11 @@ require('dotenv').config();
 
 // Конфигурация базы данных PostgreSQL
 const pool = new Pool({
-  user: process.env.TEST_USER, // Пользователь базы данных
-  host: process.env.HOST, // Хост базы данных (обычно localhost)
-  database: process.env.TEST_DB, // Название базы данных, которую мы создали
-  password: process.env.TEST_PAS, // Пароль пользователя postgres
-  port: process.env.PORT, // Порт PostgreSQL (по умолчанию 5432)
+  user: process.env.BD_USER, // Пользователь базы данных
+  host: process.env.BD_HOST, // Хост базы данных (обычно localhost)
+  database: process.env.DB, // Название базы данных, которую мы создали
+  password: process.env.BD_PAS, // Пароль пользователя postgres
+  port: process.env.DB_PORT, // Порт PostgreSQL (по умолчанию 5432)
 });
 
 module.exports = pool;
